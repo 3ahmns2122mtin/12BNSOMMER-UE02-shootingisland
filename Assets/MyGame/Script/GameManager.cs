ab083886653904e864bf9f410b0d6527d5b7f2a9
BNSOMMER <90834410+BNSOMMER@users.noreply.github.com>
2021-11-11 10:34:49 +0100

    Add files via upload

--- Assets/MyGame/Script/GameManager.cs
+++ Assets/MyGame/Script/GameManager.cs
@@ -1,42 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject target;
+    public GameObject parentOfTargets;
+    public GameObject objCounter;
+
+    private Text textCounter;
+    public bool won;
+    public int score;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(Random.Range(4, 20));
-        Debug.Log(Random.Range(4, 20));
-        Debug.Log(Random.Range(4, 20));
-        Debug.Log(Random.Range(4, 20));
+        textCounter = objCounter.GetComponent<Text>();
+        won = false;
+        InvokeRepeating("spawn", 1f,2f);
 
-        spawn();
-        spawn();
-        spawn();
-        spawn();
-        spawn();
-        spawn();
-        spawn();
-        spawn();
-        spawn();
+
 
     }
 
-
+    //spawn a Target at a random position within a specified x and y range.
+    //Instantiate (make a concrete GameObject, i.e., a clone from the given prefab Target) the 
+    //Target as a child of the ParentOfTargets. In this case transform. localPosition instead of 
+    //transform.position is important!!
     private void spawn()
     {
         //Target spawnen
-        float randomX = Random.Range(-100, 100);
-        float randomY = Random.Range(-100, 100);
+        float randomX = Random.Range(-414, 414);
+        float randomY = Random.Range(-234, 234);
 
         Vector2 random2DPosition = new Vector2(randomX, randomY);
+
+        GameObject myTarget = Instantiate(target,parentOfTargets.transform);
+        myTarget.transform.localPosition = random2DPosition;
+
+        Debug.Log(random2DPosition);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(won == true)
+        {
+            CancelInvoke("spawn");
+        }
+        else
+        {
+            Debug.Log(won);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse Pressed");
+        }
+    }
+    public void IncrementScore()
+    {
+        score++;
+        Debug.Log("Increment ..." + score);
+        textCounter.text = score.ToString();
+
+        if(score > 10)
+        {
+            won = true;
+        }
     }
 }
--- Assets/MyGame/Script/Target.cs
+++ Assets/MyGame/Script/Target.cs
@@ -3,18 +3,19 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    public int secToDestroy;
-
+    [SerializeField] private int secToDestroy;
+    private GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = FindObjectOfType<GameManager>();
         //secToDestroy = 2;
         //Destroy(gameObject, secToDestroy);
     }
 
     private void OnMouseDown()
     {
-        
+        gameManager.IncrementScore();
         Destroy(gameObject);
     }
 
